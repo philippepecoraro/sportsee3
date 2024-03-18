9fdc0846f1a1dc5ad054a863df162723f2b2a53a
Philippe Pecoraro <philippepecoraro@MinidePhilippe-001.lan>
2024-03-18 12:22:00 +0100

    responsive profil page and add tooltips

--- src/components/VerticalNavbar/VerticalNavbar.jsx
+++ src/components/VerticalNavbar/VerticalNavbar.jsx
@@ -1,37 +1,64 @@
-import React from 'react'
-import './VerticalNavbar.css'
-import { Link } from 'react-router-dom'
-import { ReactComponent as Icon1 } from '../../assets/icon1.svg'
-import { ReactComponent as Icon2 } from '../../assets/icon2.svg'
-import { ReactComponent as Icon3 } from '../../assets/icon3.svg'
-import { ReactComponent as Icon4 } from '../../assets/icon4.svg'
+import React from "react";
+import "./VerticalNavbar.css";
+import { Link } from "react-router-dom";
+import { ReactComponent as Icon1 } from "../../assets/icon1.svg";
+import { ReactComponent as Icon2 } from "../../assets/icon2.svg";
+import { ReactComponent as Icon3 } from "../../assets/icon3.svg";
+import { ReactComponent as Icon4 } from "../../assets/icon4.svg";
+import { Tooltip } from "react-tooltip";
 
 /**
- * Function VerticalNavbar - VerticalNavBar creation. 
+ * Function VerticalNavbar - VerticalNavBar creation.
  */
 const VerticalNavbar = () => {
-    return (
-        <div className='verticalNavbar'>
-            <div className='verticalNavbarIcons'>
-                <Link to={'#'} className='verticalNavbarIcon1'>
-                    <Icon1 />
-                </Link>
-                <Link to={'#'} className='verticalNavbarIcon2'>
-                    <Icon2 />
-                </Link>
-                <Link to={'#'} className='verticalNavbarIcon3'>
-                    <Icon3 />
-                </Link>
-                <Link to={'#'} className='verticalNavbarIcon4'>
-                    <Icon4 />
-                </Link>
-            </div>
+  return (
+    <div className="verticalNavbar">
+      <div className="verticalNavbarIcons">
+        <Link
+          to={"#"}
+          className="verticalNavbarIcon1"
+          data-tooltip-id="my-tooltip"
+          data-tooltip-place="top"
+          data-tooltip-content="Pas implémenté !"
+        >
+          <Tooltip id="my-tooltip" className="tooltip" />
+          <Icon1 />
+        </Link>
+        <Link
+          to={"#"}
+          className="verticalNavbarIcon2"
+          data-tooltip-id="my-tooltip"
+          data-tooltip-place="top"
+          data-tooltip-content="Pas implémenté !"
+        >
+          <Tooltip id="my-tooltip" className="tooltip" />
+          <Icon2 />
+        </Link>
+        <Link
+          to={"#"}
+          className="verticalNavbarIcon3"
+          data-tooltip-id="my-tooltip"
+          data-tooltip-place="top"
+          data-tooltip-content="Pas implémenté !"
+        >
+          <Tooltip id="my-tooltip" className="tooltip" />
+          <Icon3 />
+        </Link>
+        <Link
+          to={"#"}
+          className="verticalNavbarIcon4"
+          data-tooltip-id="my-tooltip"
+          data-tooltip-place="top"
+          data-tooltip-content="Pas implémenté !"
+        >
+          <Tooltip id="my-tooltip" className="tooltip" />
+          <Icon4 />
+        </Link>
+      </div>
 
-            <div className='verticalNavbarText'>
-                Copiryght, SportSee 2020
-            </div>
-        </div>
-    )
-}
+      <div className="verticalNavbarText">Copiryght, SportSee 2020</div>
+    </div>
+  );
+};
 
-export default VerticalNavbar
+export default VerticalNavbar;
